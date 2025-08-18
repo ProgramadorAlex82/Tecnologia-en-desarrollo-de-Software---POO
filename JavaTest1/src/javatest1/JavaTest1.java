009f6c206dbe131625fbb82599f3a173ed5399af
Alex Santiago Vanegas Suarez <134949216+ProgramadorAlex82@users.noreply.github.com>
2025-08-18 18:20:27 -0500

    Add files via upload

---
+++ JavaTest1/src/javatest1/JavaTest1.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package javatest1;
+
+/*
+Esto es un 
+Comentario en varias lineas
+*/
+
+public class JavaTest1 {
+
+    public static void main(String[] args) {
+         
+        
+        String mundo= "Mundo"; //Variable de texto con el nombre Mundo
+       System.out.println("Hola "+ mundo); //Muestra el contenido en la consola, muestra "Hola Mundo"
+       
+       System.out.println("Mi edad es de 19 años");
+       
+       System.out.println("Mi color favorito es el verde");
+       
+       System.out.println("Vivo en Neiva");
+       
+       System.err.println("Hola");
+       
+       System.out.println("""
+                                        _______
+                                   ..-'`       ````---.
+                                 .'          ___ .'````.'SS'.
+                                /        ..-SS####'.  /SSHH##'.
+                               |       .'SSSHHHH##|/#/#HH#H####'.
+                              /      .'SSHHHHH####/||#/: \\SHH#####\\
+                             /      /SSHHHHH#####/!||;`___|SSHH###\\
+                          -..__    /SSSHHH######.         \\SSSHH###\\
+                          `.'-.''--._SHHH#####.'           '.SH####/
+                            '. ``'-  '/SH####`/_             `|H##/
+                            | '.     /SSHH###|`'==.       .=='/\\H|
+                            |   `'-.|SHHHH##/\\__\\/        /\\//|~|/
+                            |    |S#|/HHH##/             |``  |
+                            |    \\H' |H#.'`              \\    |
+                            |        ''`|               -     /
+                            |          /H\\          .----    /
+                            |         |H#/'.           `    /
+                            |          \\| | '..            /
+                            |    ^~DLF   /|    ''..______.'
+                             \\          //\\__    _..-. | 
+                              \\         ||   ````     \\ |_
+                               \\    _.-|               \\| |_
+                               _\\_.-'   `'''''-.        |   `--.
+                           ''``    \\            `''-;    \\ /
+                                    \\      .-'|     ````.' -
+                                    |    .'  `--'''''-.. |/
+                                    |  .'               \\|
+                                    |.'""");
+    }
+    
+}
