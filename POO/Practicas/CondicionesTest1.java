36b1437a69c492ea793e02b697e0d80ed999cebc
Alex Santiago Vanegas Suarez <134949216+ProgramadorAlex82@users.noreply.github.com>
2025-08-19 00:08:50 -0500

    Add files via upload

---
+++ POO/Practicas/CondicionesTest1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class CondicionesTest1 {
+    public static void main(String[] args) {
+        
+        /*var edad=18;
+                
+                if (edad >=18){
+        
+                    System.out.println("Puedes Votar");
+                }*/
+        
+        /*var num1=2;
+        var num2=1;
+        
+        if (num1>num2) {
+                System.out.println("El numero 1 es mayor que el numero 2");
+        
+    } else if (num1==num2) {
+            System.out.println("Los dos son iguales");
+        } else {
+        System.out.println("El numero 2 es mayor que el numero 1");
+                }*/
+        
+        
+        
+        
+        
+                
+} 
+}    
+
